5e0b408ac0f4483036c68f59d04b70c1c32c25d0
Santiago Martín Agra <santiagomartinagra@gmail.com>
2022-02-17 22:39:36 +0100

    feat: add new 'DeleteGeniallyService' implementation

--- src/contexts/core/genially/application/DeleteGeniallyService.ts
+++ src/contexts/core/genially/application/DeleteGeniallyService.ts
@@ -1,7 +1,19 @@
 import Genially from "../domain/Genially";
+import GeniallyNotExist from "../domain/GeniallyNotExist";
+import GeniallyRepository from "../domain/GeniallyRepository";
 
 export default class DeleteGeniallyService {
-  public async execute(): Promise<Genially> {
+  public constructor(private geniallyRepository: GeniallyRepository) {}
+
+  public async execute(id: Genially["id"]): Promise<void> {
+    const genially = this.geniallyRepository.find(id);
+
+    if (!genially) {
+      throw new GeniallyNotExist(id);
+    }
+
+    await this.geniallyRepository.delete(id);
+
     return undefined;
   }
 }
